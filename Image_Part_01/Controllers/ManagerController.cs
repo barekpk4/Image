33c5e1cd1914357b2822dd71e2410946786eca4d
User <User@DESKTOP-P7K57AH>
2022-10-26 00:32:15 +0600

    Add project files.

---
+++ Image_Part_01/Controllers/ManagerController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Image_Part_01.Controllers
+{
+    public class ManagerController : Controller
+    {
+        private readonly IWebHostEnvironment _hostEnv;
+        public ManagerController(IWebHostEnvironment _hostEnv)
+        {
+            this._hostEnv = _hostEnv;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.msg = TempData["msg"];
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile fileToUpload)
+        {
+            string msg = "";
+            if(fileToUpload !=null && fileToUpload.Length > 0)
+            {
+                string webroot = _hostEnv.WebRootPath;
+                string folder = "UploadedFiles";
+                string fileName = fileToUpload.FileName;
+                string fileToWrite = Path.Combine(webroot, folder, fileName);
+
+                using (var stream = new FileStream(fileToWrite, FileMode.Create))
+                {
+                    await fileToUpload.CopyToAsync(stream);
+                    msg = "File[" + fileName + "] is uploaded successfull";
+                }
+            }
+            else
+            {
+                msg = "Please select a valid file to upload%%%";
+            }
+            TempData["msg"] = msg;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Image_Part_01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Image_Part_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
